7fd1a1508f825cf5cfdeafd82dae01354ab69c69
BAUHAUS <patrickfromgnomedk@gmail.com>
2023-04-27 14:25:53 +0200

    Ore Generation again

--- src/main/java/com/patrickfrom/moreores/MoreOres.java
+++ src/main/java/com/patrickfrom/moreores/MoreOres.java
@@ -4,25 +4,10 @@ import com.patrickfrom.moreores.block.ModBlocks;
 import com.patrickfrom.moreores.item.ModItems;
 import com.patrickfrom.moreores.world.ModPlacedFeatures;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModification;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.registry.Registerable;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.structure.rule.RuleTest;
-import net.minecraft.structure.rule.TagMatchRuleTest;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.feature.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 public class MoreOres implements ModInitializer {
-
     public static final String MOD_ID = "moreores";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
--- src/main/java/com/patrickfrom/moreores/world/ModPlacedFeatures.java
+++ src/main/java/com/patrickfrom/moreores/world/ModPlacedFeatures.java
@@ -11,17 +11,27 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 
 public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> AXOLOTL_ORE_PLACED_KEY = registerPlacedFeature("ore_axolotl");
+    public static final RegistryKey<PlacedFeature> DIRT_ORE_PLACED_KEY = registerPlacedFeature("ore_dirt");
+    public static final RegistryKey<PlacedFeature> APPLE_ORE_PLACED_KEY = registerPlacedFeature("ore_apple");
+    public static final RegistryKey<PlacedFeature> GOLDEN_APPLE_ORE_PLACED_KEY = registerPlacedFeature("ore_golden_apple");
+
+    public static void registerPlacedFeatures() {
+        MoreOres.LOGGER.info("Registering Placed Features");
+
+        addUndergroundOreFeature(AXOLOTL_ORE_PLACED_KEY);
+        addUndergroundOreFeature(DIRT_ORE_PLACED_KEY);
+        addUndergroundOreFeature(APPLE_ORE_PLACED_KEY);
+        addUndergroundOreFeature(GOLDEN_APPLE_ORE_PLACED_KEY);
+    }
 
     private static RegistryKey<PlacedFeature> registerPlacedFeature(String name) {
         return RegistryKey.of(
                 RegistryKeys.PLACED_FEATURE, new Identifier(MoreOres.MOD_ID, name));
     }
 
-    public static void registerPlacedFeatures() {
-        MoreOres.LOGGER.info("Registering Placed Features");
-
+    private static void addUndergroundOreFeature(RegistryKey<PlacedFeature> registryKey) {
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
-                GenerationStep.Feature.UNDERGROUND_ORES, AXOLOTL_ORE_PLACED_KEY);
-
+                GenerationStep.Feature.UNDERGROUND_ORES, registryKey);
     }
+
 }
